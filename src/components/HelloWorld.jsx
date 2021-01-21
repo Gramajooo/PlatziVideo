515d60ffb9e50f0c05f36a3f7d05d44eb97c4a3c
Jimena-chan <jiimenagramajo@gmail.com>
2021-01-21 16:45:20 -0600

    config gitignore, eslint and sass

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from "react";
+import "../assets/style/App.scss";
 
-const HelloWorld = () =>{
-  <h1>Hola polacos</h1>
-}
+const HelloWorld = () => <h1>Hola polacos</h1>;
 
 export default HelloWorld;
