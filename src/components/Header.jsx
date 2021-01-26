2635a0c6ed364b39c04558179811807788ccb440
Jimena-chan <jiimenagramajo@gmail.com>
2021-01-25 20:58:41 -0600

    implementing redux in login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,64 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/style/components/Header.scss';
 import logo from '../assets/img/logo-platzi-video-BW2.png';
 import userIcon from '../assets/img/user-icon.png';
 
-const Header = () => (
-  <header className="header">
-    <Link to="/">
-      <img className="header__img" src={logo} alt="Platzi Video" />
-    </Link>
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="User" />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li>
-          <a href="#">Cuenta</a>
-        </li>
-        <li>
-          <a href="#">Cerrar sesión</a>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+const Header = (props) => {
+  const { user } = props;
 
-export default Header;
+  const hasUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
+  return (
+    <header className="header">
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {hasUser ? (
+            <img src={gravatar(user.email)} alt={user.email} />
+          ) : (
+            <img src={userIcon} alt="User" />
+          )}
+          <p>Perfil</p>
+        </div>
+        <ul>
+          {hasUser ? (
+            <li>
+              <a href="/">{user.name}</a>
+            </li>
+          ) : null}
+          {hasUser ? (
+            <li>
+              <a href="#logout" onClick={handleLogout}>
+                Cerrar sesión
+              </a>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Iniciar sesión</Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+const mapDispatchProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchProps)(Header);
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,41 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { loginRequest } from '../actions';
 import '../assets/style/components/Login.scss';
 import googleIcon from '../assets/img/icons8-google-plus-50.png';
 import twitterIcon from '../assets/img/icons8-twitter-50.png';
 
-const Login = () => (
-  <section className="login">
-    <div className="login__container">
-      <h2 className="login__container--title">Login</h2>
-      <form className="login__container--form">
-        <input className="input" type="text" placeholder="Email" />
-        <input className="input" type="password" placeholder="Password" />
-        <button className="button">Login</button>
-        <div className="login__container--remember">
-          <label>
-            <input type="checkbox" name="rembe" id="rembe" value="rembe" />
-            Remember me
-          </label>
-          <a href="/">Forgot the password</a>
-        </div>
-      </form>
-      <section className="login__container--social-media">
-        <div>
-          <img src={googleIcon} alt="Google" />
-          Login with Google
-        </div>
-        <div>
-          <img src={twitterIcon} alt="Twitter" />
-          Login with Twitter
-        </div>
-      </section>
-      <p className="login__container--register">
-        You don't have any count.
-        <Link to="/register">Register</Link>
-      </p>
-    </div>
-  </section>
-);
+const Login = (props) => {
+  const [values, setValues] = useState({
+    email: '',
+  });
+  const handleInput = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.loginRequest(values);
+    props.history.push('/');
+  };
+  return (
+    <section className="login">
+      <div className="login__container">
+        <h2 className="login__container--title">Login</h2>
+        <form className="login__container--form" onSubmit={handleSubmit}>
+          <input
+            name="email"
+            className="input"
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            name="password"
+            className="input"
+            type="password"
+            placeholder="Password"
+          />
+          <button className="button">Login</button>
+          <div className="login__container--remember">
+            <label>
+              <input type="checkbox" name="rembe" id="rembe" value="rembe" />
+              Remember me
+            </label>
+            <a href="/">Forgot the password</a>
+          </div>
+        </form>
+        <section className="login__container--social-media">
+          <div>
+            <img src={googleIcon} alt="Google" />
+            Login with Google
+          </div>
+          <div>
+            <img src={twitterIcon} alt="Twitter" />
+            Login with Twitter
+          </div>
+        </section>
+        <p className="login__container--register">
+          You don't have any count.
+          <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Login;
+const mapDispatchToProps = {
+  loginRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
